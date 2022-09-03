2e5b91474d029839ed20497ff9a7fc7af424c7a3
Walter Palombini <walterpalombini@gmail.com>
2022-09-03 19:07:42 +1000

    Lazy load routes

--- src/containers/Routes/index.tsx
+++ src/containers/Routes/index.tsx
@@ -2,22 +2,59 @@ import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from 'components/ui/layout/Layout';
-import BlogPage from 'pages/blog';
-import HomePage from 'pages/home';
-import NotFoundPage from 'pages/not-found';
-import PricingPage from 'pages/pricing';
-import ProductsPage from 'pages/products';
+import LoadingPage from 'pages/loading';
+
+const BlogPage = React.lazy(() => import('pages/blog'));
+const HomePage = React.lazy(() => import('pages/home'));
+const NotFoundPage = React.lazy(() => import('pages/not-found'));
+const PricingPage = React.lazy(() => import('pages/pricing'));
+const ProductsPage = React.lazy(() => import('pages/products'));
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="products" element={<ProductsPage />} />
-          <Route path="pricing" element={<PricingPage />} />
-          <Route path="blog" element={<BlogPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route
+            index
+            element={
+              <React.Suspense fallback={<LoadingPage />}>
+                <HomePage />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="products"
+            element={
+              <React.Suspense fallback={<LoadingPage />}>
+                <ProductsPage />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="pricing"
+            element={
+              <React.Suspense fallback={<LoadingPage />}>
+                <PricingPage />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="blog"
+            element={
+              <React.Suspense fallback={<LoadingPage />}>
+                <BlogPage />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <React.Suspense fallback={<LoadingPage />}>
+                <NotFoundPage />
+              </React.Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
